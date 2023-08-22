a7679d747b3b84cfdd6a4c47d45ac120f510f7e4
Vega <2187815@cognizant.com>
2023-08-22 08:37:12 -0300

    First commit.

---
+++ BlazorWebAssemblyDemo/Client/Models/TaskModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebAssemblyDemo.Client.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+    }
+}
